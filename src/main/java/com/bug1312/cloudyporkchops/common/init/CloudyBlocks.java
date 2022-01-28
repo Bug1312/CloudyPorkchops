2c63f96228a4bbbc33ce273740406247fa5f6a56
Bug1312 <bug1312.babycarrots1312@gmail.com>
2022-01-28 09:51:36 -0500

    cleanup CloudyBlocks methods

--- src/main/java/com/bug1312/cloudyporkchops/common/init/CloudyBlocks.java
+++ src/main/java/com/bug1312/cloudyporkchops/common/init/CloudyBlocks.java
@@ -8,8 +8,6 @@ import net.minecraft.block.AbstractBlock.Properties;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -19,7 +17,7 @@ public class CloudyBlocks {
 	public static RegistryObject<Block> INVISIBLE_TABLE = registerBlock(new InvisibleTable(Properties.of(Material.WOOD).noOcclusion()), "invisible_table", true);
 	public static RegistryObject<Block> SPRAY_ON = registerBlock(new SprayOnBlock(Properties.of(Material.CLAY, MaterialColor.COLOR_PURPLE).noOcclusion().strength(50.0F, 1200.0F)), "elastic_biopolymer_adhesive", false);
 	
-	/* Register Normal Block */
+	/* Register */
 	public static RegistryObject<Block> registerBlock(Block block, String name, Item.Properties properties, boolean hasItem) {
 		if (hasItem) {
 			RegistryHandler.ITEMS.register(name, () -> new BlockItem(block, properties));
@@ -29,11 +27,5 @@ public class CloudyBlocks {
 	
 	public static RegistryObject<Block> registerBlock(Block block, String name, boolean hasItem) {
 		return registerBlock(block, name, new Item.Properties(), hasItem);
-	}
-	
-	/* Register a render type for transparent textures on a json model */
-	public static void registerRenderType(Block block, RenderType renderType) {
-		RenderTypeLookup.setRenderLayer(block, renderType);
-	}
-
+	}	
 }
