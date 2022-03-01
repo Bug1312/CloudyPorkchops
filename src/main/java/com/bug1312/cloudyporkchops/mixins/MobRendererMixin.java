068b5269389579f3af6347bd31d2cd6a57709e5a
Bug1312 <bug1312.babycarrots1312@gmail.com>
2022-03-01 09:22:40 -0500

    some mixin testing; future me: fix it

---
+++ src/main/java/com/bug1312/cloudyporkchops/mixins/MobRendererMixin.java
@@ -0,0 +1,20 @@
+package com.bug1312.cloudyporkchops.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.model.BatModel;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.entity.MobEntity;
+
+@Mixin(MobRenderer.class)
+public class MobRendererMixin<T extends MobEntity, M extends EntityModel<T>> {
+	@SuppressWarnings("unchecked")
+	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/renderer/entity/MobRenderer;getModel()Lnet/minecraft/client/renderer/entity/model/EntityModel;", cancellable = true)
+	private void getModel(CallbackInfoReturnable<M> info) {
+		info.setReturnValue((M) new BatModel());
+	}
+}
