c4621f12523198bad2377b41f6a0673c202d7b87
Bug1312 <bug1312.babycarrots1312@gmail.com>
2022-03-01 09:36:14 -0500

    more testy testy

--- src/main/java/com/bug1312/cloudyporkchops/mixins/MobRendererMixin.java
+++ src/main/java/com/bug1312/cloudyporkchops/mixins/MobRendererMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.model.BatModel;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.entity.MobEntity;
+import net.minecraft.util.ResourceLocation;
 
 @Mixin(MobRenderer.class)
 public class MobRendererMixin<T extends MobEntity, M extends EntityModel<T>> {
@@ -16,5 +17,10 @@ public class MobRendererMixin<T extends MobEntity, M extends EntityModel<T>> {
 	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/renderer/entity/MobRenderer;getModel()Lnet/minecraft/client/renderer/entity/model/EntityModel;", cancellable = true)
 	private void getModel(CallbackInfoReturnable<M> info) {
 		info.setReturnValue((M) new BatModel());
+	}
+	
+	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/renderer/entity/MobRenderer;getTextureLocation(Lnet/minecraft/entity/Entity;)Lnet/minecraft/util/ResourceLocation;", cancellable = true)
+	private void getTextureLocation(CallbackInfoReturnable<ResourceLocation> info) {
+		info.setReturnValue(new ResourceLocation("textures/entity/cow.png"));
 	}
 }
