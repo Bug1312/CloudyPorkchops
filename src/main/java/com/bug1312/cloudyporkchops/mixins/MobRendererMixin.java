73302de9fff0544b34e290a0651ae2d128fee553
Bug1312 <bug1312.babycarrots1312@gmail.com>
2022-03-02 09:54:36 -0500

    mixinasfsda

--- src/main/java/com/bug1312/cloudyporkchops/mixins/MobRendererMixin.java
+++ src/main/java/com/bug1312/cloudyporkchops/mixins/MobRendererMixin.java
@@ -5,22 +5,23 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.client.renderer.entity.model.BatModel;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.entity.MobEntity;
-import net.minecraft.util.ResourceLocation;
 
-@Mixin(MobRenderer.class)
+@Mixin(LivingRenderer.class)
 public class MobRendererMixin<T extends MobEntity, M extends EntityModel<T>> {
 	@SuppressWarnings("unchecked")
-	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/renderer/entity/MobRenderer;getModel()Lnet/minecraft/client/renderer/entity/model/EntityModel;", cancellable = true)
+	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/renderer/entity/LivingRenderer;getModel()Lnet/minecraft/client/renderer/entity/model/EntityModel;", cancellable= true)
 	private void getModel(CallbackInfoReturnable<M> info) {
-		info.setReturnValue((M) new BatModel());
+		if(info.getReturnValue() != null)
+		System.out.println(info.getReturnValue().toString());
+//		info.setReturnValue((M) new BatModel());
 	}
 	
-	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/renderer/entity/MobRenderer;getTextureLocation(Lnet/minecraft/entity/Entity;)Lnet/minecraft/util/ResourceLocation;", cancellable = true)
-	private void getTextureLocation(CallbackInfoReturnable<ResourceLocation> info) {
-		info.setReturnValue(new ResourceLocation("textures/entity/cow.png"));
-	}
+//	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/renderer/entity/LivingRenderer;getTextureLocation(Lnet/minecraft/entity/Entity;)Lnet/minecraft/util/ResourceLocation;", cancellable = true)
+//	private void getTextureLocation(CallbackInfoReturnable<ResourceLocation> info) {
+//		info.setReturnValue(new ResourceLocation("textures/entity/cow.png"));
+//	}
 }
