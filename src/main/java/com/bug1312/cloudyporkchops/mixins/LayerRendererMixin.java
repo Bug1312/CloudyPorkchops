3a8a8b10bedd417c742aa44bd61d2c46228b5f74
Bug1312 <bug1312.babycarrots1312@gmail.com>
2022-03-04 08:47:07 -0500

    mixins fricken frackin working and efficient

--- src/main/java/com/bug1312/cloudyporkchops/mixins/LayerRendererMixin.java
+++ src/main/java/com/bug1312/cloudyporkchops/mixins/LayerRendererMixin.java
@@ -2,16 +2,13 @@ package com.bug1312.cloudyporkchops.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import com.bug1312.cloudyporkchops.util.SprayEntityHelper;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.entity.Entity;
@@ -20,14 +17,11 @@ import net.minecraft.util.ResourceLocation;
 
 @Mixin(LayerRenderer.class)
 public class LayerRendererMixin<T extends Entity, M extends EntityModel<T>> {
-
-	@Inject(at = @At("HEAD"), cancellable = true, method = "Lnet/minecraft/client/renderer/entity/layers/LayerRenderer;renderColoredCutoutModel(Lnet/minecraft/client/renderer/entity/model/EntityModel;Lnet/minecraft/util/ResourceLocation;Lcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer;ILnet/minecraft/entity/LivingEntity;FFF)V")
-	private static <T extends LivingEntity> void renderColoredCutoutModel(EntityModel<T> m, ResourceLocation rl, MatrixStack s, IRenderTypeBuffer bf, int n_0, T entity, float n_1, float n_2, float n_3, CallbackInfo info) {
-		if (SprayEntityHelper.isEntitySprayedOn(entity)) {
-			IVertexBuilder ivertexbuilder = bf.getBuffer(RenderType.entityCutoutNoCull(SprayEntityHelper.texture));
-			m.renderToBuffer(s, ivertexbuilder, n_0, LivingRenderer.getOverlayCoords(entity, 0.0F), n_1, n_2, n_3, 1.0F);
-			info.cancel();
-		}
+	
+	@ModifyVariable(at = @At("STORE"), ordinal = 0, method = "Lnet/minecraft/client/renderer/entity/layers/LayerRenderer;renderColoredCutoutModel(Lnet/minecraft/client/renderer/entity/model/EntityModel;Lnet/minecraft/util/ResourceLocation;Lcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer;ILnet/minecraft/entity/LivingEntity;FFF)V")
+	private static <T extends LivingEntity> IVertexBuilder swapTexture(IVertexBuilder def, EntityModel<T> u_0, ResourceLocation u_1, MatrixStack u_2, IRenderTypeBuffer buffer, int u_3, T entity) {
+		if(SprayEntityHelper.isEntitySprayedOn(entity)) return buffer.getBuffer(RenderType.entityCutoutNoCull(SprayEntityHelper.texture));
+		return def;
 	}
 
 }
