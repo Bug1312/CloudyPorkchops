cf52746f32895fa197d43e408089032bb0b7df13
Bug1312 <bug1312.babycarrots1312@gmail.com>
2022-03-25 09:43:47 -0400

    revert one file

--- src/main/java/com/bug1312/cloudyporkchops/util/IHorizontalBlock.java
+++ src/main/java/com/bug1312/cloudyporkchops/util/IHorizontalBlock.java
@@ -1,4 +1,4 @@
-package com.bug1312.cloudyporkchops.util.helpers;
+package com.bug1312.cloudyporkchops.util;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.state.properties.BlockStateProperties;
